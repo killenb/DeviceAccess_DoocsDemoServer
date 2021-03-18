8f501f67a3c3debe90dc535d4b8d872cc95ca344
Martin Killenberg <martin.killenberg@desy.de>
2021-03-18 17:03:00 +0100

    server with exception handling

--- src/EqFctOven.cpp
+++ src/EqFctOven.cpp
@@ -3,16 +3,24 @@
 void EqFctOven::update() {
   static const double gain = 100.0;
 
-  if(!oven.isOpened()) {
-    oven.open();
+  try {
+    if(!oven.isFunctional()) {
+      oven.open();
+    }
+
+    // a simple proportional controller
+    hwTemperature.read();
+    hwHeatingCurrent = gain * (setpoint.value() - hwTemperature);
+    hwHeatingCurrent.write();
+
+    // No exception until here -> Device is working. Clear the error flag.
+    set_error(no_error);
+
+    // update the doocs properties
+    temperature.set_value(hwTemperature);
+    heatingCurrent.set_value(hwHeatingCurrent);
   }
-
-  // a simple proportional controller
-  hwTemperature.read();
-  hwHeatingCurrent = gain * (setpoint.value() - hwTemperature);
-  hwHeatingCurrent.write();
-
-  // update the doocs properties
-  temperature.set_value(hwTemperature);
-  heatingCurrent.set_value(hwHeatingCurrent);
+  catch(ctk::runtime_error& e) {
+    set_error(device_error, e.what());
+  }
 }
